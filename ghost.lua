d18c33cfa474701dd9a8a70da72cda304ed2dc04
Diegolasnew <solollamamepatodnald@gmail.com>
2013-04-21 02:06:12 -0400

    Ejemplo de orientación a objetos
    ademas una imagen del fantasma

---
+++ ghost.lua
@@ -0,0 +1,50 @@
+Ghost = {}
+Ghost.__index = Ghost
+
+function Ghost:new(x, y)
+   local gst = {}            
+   setmetatable(gst,Ghost)
+   gst.x = x
+   gst.y = y
+   gst.vx = 200
+   gst.vy = 200
+   gst.sprite = gfx.newImage("img/ghost1.png")
+   gst.dt = 0
+   return gst
+end
+
+function Ghost:up(  )
+	self.y = self.y - self.vy*self.dt
+end
+function Ghost:down(  )
+	self.y = self.y + self.vy*self.dt
+end
+function Ghost:left(  )
+	self.x = self.x - self.vx*self.dt
+end
+function Ghost:right(  )
+	self.x = self.x + self.vx*self.dt
+end
+
+
+
+function Ghost:update( dt )
+	self.dt = dt
+	if love.keyboard.isDown("up") then
+		self:up()
+	end
+	if love.keyboard.isDown("down") then
+		self:down()
+	end
+	if love.keyboard.isDown("left") then
+		self:left()
+	end
+	if love.keyboard.isDown("right") then
+		self:right()
+	end
+
+end
+
+function Ghost:draw()
+	gfx.draw(self.sprite, self.x, self.y)
+end
--- main.lua
+++ main.lua
@@ -1,14 +1,28 @@
 gfx = love.graphics
 fsy = love.filesystem
+defColor={255,255,255}
+
+
+require("log")
+require("ghost")
+
 
 function love.load( )
-	
+	log = true;
+	fant = Ghost:new(50,50)
+	gfx.setBackgroundColor(255,255,255)
 end
 
 function love.update( dt )
-	
+	logUpdate( dt )
+	fant:update(dt)
 end
 
-function love.draw(  )
-	gfx.print("hola mundo", 100, 100)
+function love.draw( )
+	gfx.setColor(defColor)
+	fant:draw()
+	if log then
+		logDraw( )
+	end
+
 end
